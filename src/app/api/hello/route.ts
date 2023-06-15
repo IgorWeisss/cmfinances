92bb79dcecfc756bb33253db710dd1a0a1efdac9
Igor - Ubuntu <igorweiss@hotmail.com.br>
2023-06-15 16:34:22 -0300

    First commit

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// Getting search params from url
+export async function GET(req:NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const query = searchParams.get('param')
+  return NextResponse.json(`Returning ${query}`)  
+}
+
+// Getting body content on POST request
+export async function POST(req:NextRequest) {
+  const body = await req.json()
+  return NextResponse.json(body)
+}
