3bccc1d241e15c48d9b4cf65ed1e86aa8e2afa30
Igor - Ubuntu <igorweiss@hotmail.com.br>
2023-06-16 16:04:51 -0300

    Create Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req:NextRequest, res:NextResponse) {
-  const url = req.url
-  console.log(url)  
+  const origin = req.nextUrl.origin
+  console.log("Origin: ", origin)
+  const pathname = req.nextUrl.pathname
+  console.log("pathname: ", pathname)
+  const basePath = req.nextUrl.basePath
+  console.log("basePath: ", basePath)
+
+  return NextResponse.next()
 }
 
 export const config = {
